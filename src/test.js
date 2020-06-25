e76cd6400b139658c68797f0483b13ab1baa4e90
RBranco1 <raphael.pieroni@etecia.com.br>
2020-06-24 22:41:31 -0300

    Fazer o OAuth2.0

---
+++ src/test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Discord = require('discord.js');
+
+const Tony = new Discord.WebhookClient('725459542384836719', 'FQRXsk_1v0rxs-ebx03yEaDyxfxfu45vnEcpgt0VmaB9z54a2KdFFpNfBKY4qrCrMRuB');
+const ChrisQ = new Discord.WebhookClient('725497950788386837', 'kyW9IFehuRhE79k9jmgCUT1FrmFOcpfAGsXSnAG-DR1xMuOJd_DUfuGW59JHP-OA8zAo');
+const Alexandre = new Discord.WebhookClient('725498023555104861', 'jXhhBlsLjQrMetqtcUssgChDSb8L6ZEVbu237r-o86g4M_6KoQdgIx6a9RjNJToOKO3o');
+const Talita = new Discord.WebhookClient('725498298722680845', '51aP38X372pZewqMLXikZVnwYr3cPj37Ttp2oMHcKKOVRCtMdHz4C5ebO6XVJWeZiJMw');
+const Leo = new Discord.WebhookClient('725498396093317211', 'DUUEY6FqGgC_iS9P7X0Fzg8rTf_osO6wWB2xhTbQELN6LyY-21dPJon6RIE7YsXIhBS0');
+const Boris = new Discord.WebhookClient('725498504184856577', 'NB2oa41A6ZmuToQHy9KEu6wXLtd3JUQBah5Lv3E80me5ue3BIZXapqp8ezUHqzF_M_nV');
+const Elaine = new Discord.WebhookClient('725498592047005917', 'n7ABhg3SRrpiCGJuet3lJLVW_RQZPYFyHiAFU55qZb_xsqPxKR9yty2257XwhOHKzc8-');
+const ChrisE = new Discord.WebhookClient('725498661941018705', 'bj3kidsGvkCDZNR-MV1GqAQA7U4BwVcJdSo8k41_F8xGYWeWcS61-TjW9uLvXDJPyX4X');
+const texto = "vamos dominar o mundo"
+
+Tony.send(texto);
+ChrisQ.send(texto);
+Alexandre.send(texto);
+Talita.send(texto);
+Leo.send(texto);
+Boris.send(texto);
+Elaine.send(texto);
+ChrisE.send(texto);
+
+
+
+
+
+
+
+
+//tony = https://discordapp.com/api/webhooks/725459542384836719/FQRXsk_1v0rxs-ebx03yEaDyxfxfu45vnEcpgt0VmaB9z54a2KdFFpNfBKY4qrCrMRuB
+//ChrisQ = https://discordapp.com/api/webhooks/725497950788386837/kyW9IFehuRhE79k9jmgCUT1FrmFOcpfAGsXSnAG-DR1xMuOJd_DUfuGW59JHP-OA8zAo
+//Alexandre = https://discordapp.com/api/webhooks/725498023555104861/jXhhBlsLjQrMetqtcUssgChDSb8L6ZEVbu237r-o86g4M_6KoQdgIx6a9RjNJToOKO3o
+
+//Talita = https://discordapp.com/api/webhooks/725498298722680845/51aP38X372pZewqMLXikZVnwYr3cPj37Ttp2oMHcKKOVRCtMdHz4C5ebO6XVJWeZiJMw
+
+//leo = https://discordapp.com/api/webhooks/725498396093317211/DUUEY6FqGgC_iS9P7X0Fzg8rTf_osO6wWB2xhTbQELN6LyY-21dPJon6RIE7YsXIhBS0
+
+//Boris = https://discordapp.com/api/webhooks/725498504184856577/NB2oa41A6ZmuToQHy9KEu6wXLtd3JUQBah5Lv3E80me5ue3BIZXapqp8ezUHqzF_M_nV
+
+//Elaine = https://discordapp.com/api/webhooks/725498592047005917/n7ABhg3SRrpiCGJuet3lJLVW_RQZPYFyHiAFU55qZb_xsqPxKR9yty2257XwhOHKzc8-
+//ChrisE = https://discordapp.com/api/webhooks/725498661941018705/bj3kidsGvkCDZNR-MV1GqAQA7U4BwVcJdSo8k41_F8xGYWeWcS61-TjW9uLvXDJPyX4X
+
+
