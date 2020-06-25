c0d109b0139cf6ea70782fa5d7e807440708c70d
RBranco1 <raphael.pieroni@etecia.com.br>
2020-06-25 15:41:33 -0300

    configurar todos os bots no listWorks

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,20 @@
 'use strict';
-
+const {google} = require('googleapis');
+const classroom = google.classroom('v1');
 const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const TOKEN = 'token.json';
+const request = require('request');
+const http = require('http');
+
+  request(`https://classroom.googleapis.com/v1/courses/24205361123/courseWork?key=AIzaSyAw3fnApXs__LstjV7v0xypyFUu74E-h84/authorization=${TOKEN}`,
+  (error, response, body) =>{
+    console.log(body)
+  });
+
+
+
+
 
 const Tony = new Discord.WebhookClient('725459542384836719', 'FQRXsk_1v0rxs-ebx03yEaDyxfxfu45vnEcpgt0VmaB9z54a2KdFFpNfBKY4qrCrMRuB');
 const ChrisQ = new Discord.WebhookClient('725497950788386837', 'kyW9IFehuRhE79k9jmgCUT1FrmFOcpfAGsXSnAG-DR1xMuOJd_DUfuGW59JHP-OA8zAo');
@@ -10,35 +24,59 @@ const Leo = new Discord.WebhookClient('725498396093317211', 'DUUEY6FqGgC_iS9P7X0
 const Boris = new Discord.WebhookClient('725498504184856577', 'NB2oa41A6ZmuToQHy9KEu6wXLtd3JUQBah5Lv3E80me5ue3BIZXapqp8ezUHqzF_M_nV');
 const Elaine = new Discord.WebhookClient('725498592047005917', 'n7ABhg3SRrpiCGJuet3lJLVW_RQZPYFyHiAFU55qZb_xsqPxKR9yty2257XwhOHKzc8-');
 const ChrisE = new Discord.WebhookClient('725498661941018705', 'bj3kidsGvkCDZNR-MV1GqAQA7U4BwVcJdSo8k41_F8xGYWeWcS61-TjW9uLvXDJPyX4X');
-const texto = "vamos dominar o mundo"
 
-Tony.send(texto);
-ChrisQ.send(texto);
-Alexandre.send(texto);
-Talita.send(texto);
-Leo.send(texto);
-Boris.send(texto);
-Elaine.send(texto);
-ChrisE.send(texto);
+
+
+const embed = new MessageEmbed()
+.setTitle('Tarefa')
+.setColor(0xffffff)
+.setDescription('Descrição de tarefa foda link');
 
 
 
 
 
+/* Tony.send(embed);
+ChrisQ.send(embed);
+Alexandre.send(embed);
+Talita.send(embed);
+Leo.send(embed);
+Boris.send(embed);
+Elaine.send(embed);
+ChrisE.send(embed); */
 
+
+
+
+
+
+//Rota para listar tarefas https://classroom.googleapis.com/v1/courses/24205361123/courseWork?key=AIzaSyAw3fnApXs__LstjV7v0xypyFUu74E-h84 HTTP/1.1
+
+//Authorization: Bearer [YOUR_ACCESS_TOKEN]
+//Accept: application/json
 
 
 //tony = https://discordapp.com/api/webhooks/725459542384836719/FQRXsk_1v0rxs-ebx03yEaDyxfxfu45vnEcpgt0VmaB9z54a2KdFFpNfBKY4qrCrMRuB
+//courseid:24205361123
+
 //ChrisQ = https://discordapp.com/api/webhooks/725497950788386837/kyW9IFehuRhE79k9jmgCUT1FrmFOcpfAGsXSnAG-DR1xMuOJd_DUfuGW59JHP-OA8zAo
+//courseid:60382416186
+
 //Alexandre = https://discordapp.com/api/webhooks/725498023555104861/jXhhBlsLjQrMetqtcUssgChDSb8L6ZEVbu237r-o86g4M_6KoQdgIx6a9RjNJToOKO3o
+//courseid:60369455656
 
 //Talita = https://discordapp.com/api/webhooks/725498298722680845/51aP38X372pZewqMLXikZVnwYr3cPj37Ttp2oMHcKKOVRCtMdHz4C5ebO6XVJWeZiJMw
+//courseid:60373040356
 
 //leo = https://discordapp.com/api/webhooks/725498396093317211/DUUEY6FqGgC_iS9P7X0Fzg8rTf_osO6wWB2xhTbQELN6LyY-21dPJon6RIE7YsXIhBS0
+//courseid:60327930072
 
 //Boris = https://discordapp.com/api/webhooks/725498504184856577/NB2oa41A6ZmuToQHy9KEu6wXLtd3JUQBah5Lv3E80me5ue3BIZXapqp8ezUHqzF_M_nV
+//courseid:61746734185
 
 //Elaine = https://discordapp.com/api/webhooks/725498592047005917/n7ABhg3SRrpiCGJuet3lJLVW_RQZPYFyHiAFU55qZb_xsqPxKR9yty2257XwhOHKzc8-
-//ChrisE = https://discordapp.com/api/webhooks/725498661941018705/bj3kidsGvkCDZNR-MV1GqAQA7U4BwVcJdSo8k41_F8xGYWeWcS61-TjW9uLvXDJPyX4X
+//courseid:60439137882
 
+//ChrisE = https://discordapp.com/api/webhooks/725498661941018705/bj3kidsGvkCDZNR-MV1GqAQA7U4BwVcJdSo8k41_F8xGYWeWcS61-TjW9uLvXDJPyX4X
+//courseid:60421191545
 
